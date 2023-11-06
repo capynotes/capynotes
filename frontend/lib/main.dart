97279ab6939742e92c3e0b8462aef52063b56b08
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2023-11-06 17:40:22 +0300

    router setup

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:capynotes/navigation/app_router.dart';
 import 'package:capynotes/view/screens/flashcard_display_screen.dart';
 import 'package:capynotes/viewmodel/flashcard_cubit/flashcard_cubit.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  static final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -18,13 +19,13 @@ class MyApp extends StatelessWidget {
           create: (context) => FlashcardCubit(),
         )
       ],
-      child: MaterialApp(
+      child: MaterialApp.router(
+        routerConfig: _appRouter.config(),
         title: 'CapyNotes',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: const FlashcardScreen(),
       ),
     );
   }
