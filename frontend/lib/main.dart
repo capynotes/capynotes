0348f2adde3be3babc5d55815d61602859491550
ismail-deniz <emre.deniz@ug.bilkent.edu.tr>
2023-11-23 16:08:03 +0300

    login screen

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -48,6 +48,7 @@ class MyApp extends StatelessWidget {
         locale: context.locale,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          primarySwatch: Colors.deepPurple,
         ),
       ),
     );
