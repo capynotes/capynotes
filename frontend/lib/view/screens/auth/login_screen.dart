185a2b663c4d75cf0c81192629eec2854a517afe
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2023-11-27 21:16:37 +0300

    init connecting

--- frontend/lib/view/screens/auth/login_screen.dart
+++ frontend/lib/view/screens/auth/login_screen.dart
@@ -1,5 +1,12 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:capynotes/view/widgets/custom_widgets/custom_snackbars.dart';
+import 'package:capynotes/viewmodel/auth/login/login_cubit.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../navigation/app_router.dart';
+import '../../../translations/locale_keys.g.dart';
 
 @RoutePage()
 class LoginScreen extends StatefulWidget {
@@ -10,55 +17,66 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  // final TextEditingController _emailController = TextEditingController();
+  // final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+      appBar: AppBar(title: Text(LocaleKeys.appbars_titles_login.tr())),
+      body: BlocConsumer<LoginCubit, LoginState>(
+        listener: (context, state) {
+          if (state is LoginSuccess) {
+            CustomSnackbars.displaySuccessMotionToast(
+                context, state.title, state.description, () {});
+          } else if (state is LoginError) {
+            CustomSnackbars.displayErrorMotionToast(
+                context, state.title, state.description, () {});
+          }
+        },
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
+                TextField(
+                  controller: context.read<LoginCubit>().emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: context.read<LoginCubit>().passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        context.router.push(const ForgotPasswordRoute());
+                      },
+                      child: const Text('Forgot Password?'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
                   onPressed: () {
-                    // TODO: Implement forgot password logic
+                    context.read<LoginCubit>().login();
                   },
-                  child: const Text('Forgot Password?'),
+                  child: const Text('Login'),
                 ),
               ],
             ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Implement login logic
-              },
-              child: const Text('Login'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
