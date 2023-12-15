4ceb8266689a8d5474d8ba670b907d31141965a4
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2023-12-15 23:50:59 +0300

    added transcription from youtube link

--- speech/app/routes.py
+++ speech/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app.transcribe import transcribe_audio
+from app.video_to_mp4 import video_to_mp4
 
 main_bp = Blueprint('main', __name__)
 
@@ -14,5 +15,22 @@ def transcribe_route():
     # Transcribe the audio file
     transcription = transcribe_audio(file_name)
 
+    # Return the transcription in the response
+    return jsonify({'transcription': transcription})
+
+@main_bp.route('/youtube', methods=['POST'])
+def youtube_route():
+    # Receive information about the video from Java Spring
+    data = request.get_json()
+
+    # Extract necessary information (e.g., url) from the data
+    video_url = data.get('url')
+
+    # convert to mp4
+    object_name = video_to_mp4(video_url)
+    if object_name:
+        transcription = transcribe_audio(object_name)
+    else:
+        transcription = "An Error Occurred. Please Try Again Later."
     # Return the transcription in the response
     return jsonify({'transcription': transcription})
