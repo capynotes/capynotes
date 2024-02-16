ff5af0f9b2c74afdf79d08bff16d9c598d57fa39
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2024-02-16 17:37:29 +0300

    added view pdf screen with dummy pdf

--- frontend/lib/utility/utils.dart
+++ frontend/lib/utility/utils.dart
@@ -1,5 +1,13 @@
+import 'package:url_launcher/url_launcher.dart';
+
 class Utils {
   static String dateToString(DateTime date) {
     return "${date.day}/${date.month}/${date.year}";
+  }
+
+  static Future<void> launchURL(String url) async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
