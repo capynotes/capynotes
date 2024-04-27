1842135780d86b55db33fb92b7ced66b6785f930
ismail-deniz <emre.deniz@ug.bilkent.edu.tr>
2024-04-27 17:38:11 +0300

    rabbitmq to sqs

--- backend/speech/receive.py
+++ backend/speech/receive.py
@@ -2,85 +2,66 @@
 import os
 import time
 import sys
-import pika
+import aws_utils
+import boto3
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 from database import get_note_from_database, insert_transcription, insert_timestamps
 from transcribe import whisper_transcribe_audio
 
-connection_send = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
-channel_send = connection_send.channel()
-channel_send.queue_declare(queue="summarization_queue")
+# Setup SQS client
+sqs = aws_utils.create_sqs_client()
+summarization_queue_url = 'summarization'
+transcription_queue_url = 'transcription'
 
 def send_to_summarize(transcription_id):
-    global connection_send, channel_send
-
     while True:
         try:
-            if not connection_send or connection_send.is_closed:
-                connection_send = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
-                channel_send = connection_send.channel()
-                channel_send.queue_declare(queue="summarization_queue")
-
-            channel_send.basic_publish(exchange="", routing_key="summarization_queue", body=str(transcription_id))
+            # Send message to SQS summarization queue
+            sqs.send_message(QueueUrl=summarization_queue_url, MessageBody=str(transcription_id))
             print(" [x] Sent ", str(transcription_id))
             break
-
-        except pika.exceptions.StreamLostError as e:
-            print(f"Stream connection lost: {e}")
+        except (NoCredentialsError, PartialCredentialsError) as e:
+            print(f"Credentials error: {e}")
             time.sleep(5)
-
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
             time.sleep(5)
 
-def callback_recv(ch, method, properties, body):
-    print(" [x] Received ", str(body))
-    note_id = int(body.decode())
-    note_data = get_note_from_database(note_id)
-    audio_file_name = note_data[5]
+def callback_recv(messages):
+    for message in messages:
+        print(" [x] Received ", str(message['Body']))
+        note_id = int(message['Body'])
+        note_data = get_note_from_database(note_id)
+        audio_file_name = note_data[5]
 
-    result = whisper_transcribe_audio(audio_file_name)
-
-    transcription = result['text']
-    segments = result['segments']
-    transcription_id_value = insert_transcription(note_id, transcription)
-    transformed_list = [{'transcription_id': transcription_id_value,
-                    'start': item['start'],
-                    'finish': item['end'],
-                    'phrase': item['text']}
-                    for item in segments]
-    insert_timestamps(transformed_list)
-    send_to_summarize(transcription_id_value)
+        result = whisper_transcribe_audio(audio_file_name)
+        transcription = result['text']
+        segments = result['segments']
+        transcription_id_value = insert_transcription(note_id, transcription)
+        transformed_list = [{'transcription_id': transcription_id_value,
+                            'start': item['start'],
+                            'finish': item['end'],
+                            'phrase': item['text']}
+                            for item in segments]
+        insert_timestamps(transformed_list)
+        send_to_summarize(transcription_id_value)
+        # Delete message from queue after processing
+        sqs.delete_message(QueueUrl=transcription_queue_url, ReceiptHandle=message['ReceiptHandle'])
 
 def main():
     while True:
         try:
-            connection_recv = pika.BlockingConnection(
-                pika.ConnectionParameters(host="rabbitmq"),
-            )
-            channel_recv = connection_recv.channel()
-            channel_recv.queue_declare(queue="transcription_queue")
-            channel_recv.basic_consume(
-                queue="transcription_queue",
-                on_message_callback=callback_recv,
-                auto_ack=True,
-            )
-
-            print(" [*] Waiting for messages. To exit press CTRL+C")
-            channel_recv.start_consuming()
-
-        except pika.exceptions.StreamLostError as e:
-            print(f"Stream connection lost: {e}")
-            print("Reconnecting...")
-            time.sleep(5)
-            continue
-
+            # Receive message from SQS transcription queue
+            response = sqs.receive_message(QueueUrl=transcription_queue_url, MaxNumberOfMessages=10, WaitTimeSeconds=20)
+            if 'Messages' in response:
+                callback_recv(response['Messages'])
+            else:
+                print(" [*] Waiting for messages. To exit press CTRL+C")
         except KeyboardInterrupt:
             print("Interrupted")
             break
-
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
-            break
 
 if __name__ == "__main__":
     try:
