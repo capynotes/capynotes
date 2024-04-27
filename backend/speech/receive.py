9702a8256820f056430bd269cf5227e9bcb97c9f
ismail-deniz <emre.deniz@ug.bilkent.edu.tr>
2024-04-27 18:02:09 +0300

    test

--- backend/speech/receive.py
+++ backend/speech/receive.py
@@ -10,8 +10,8 @@ from transcribe import whisper_transcribe_audio
 
 # Setup SQS client
 sqs = aws_utils.create_sqs_client()
-summarization_queue_url = 'summarization'
-transcription_queue_url = 'transcription'
+summarization_queue_url = 'https://sqs.eu-west-2.amazonaws.com/101807873666/summarization'
+transcription_queue_url = 'https://sqs.eu-west-2.amazonaws.com/101807873666/transcription'
 
 def send_to_summarize(transcription_id):
     while True:
