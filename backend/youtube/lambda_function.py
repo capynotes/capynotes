dfb6789ddf385c086c898bc1e5650bb4bb829d6e
ismail-deniz <emre.deniz@ug.bilkent.edu.tr>
2024-05-07 21:45:38 +0300

    youtube lambda

---
+++ backend/youtube/lambda_function.py
@@ -0,0 +1,51 @@
+import json
+import boto3
+from video_to_mp4 import video_to_mp4
+from database import update_s3_name
+import aws_utils
+
+# Setup SQS client
+sqs = aws_utils.create_sqs_client()
+youtube_queue_url = 'https://sqs.us-east-1.amazonaws.com/211125669571/youtube'
+transcription_queue_url = 'https://sqs.us-east-1.amazonaws.com/211125669571/transcription'
+
+def send_to_speech(note_id):
+    try:
+        # Send message to SQS transcription queue
+        sqs.send_message(QueueUrl=transcription_queue_url, MessageBody=str(note_id))
+        print(" [x] Sent ", str(note_id))
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+def process_message(message):
+    print(" [x] Received ", message['Body'])
+    yt_json_data = json.loads(message['Body'])
+
+    note_id = yt_json_data["noteId"]
+    video_url = yt_json_data["videoUrl"]
+    note_name = yt_json_data["noteName"]
+
+    file_name = video_to_mp4(video_url, note_name)
+    if file_name:
+        update_s3_name(note_id, file_name)
+        send_to_speech(int(note_id))
+    else:
+        print("An Error Occurred. Please Try Again Later.")
+
+def lambda_handler(event, context):
+    # Process each message from SQS
+    for record in event['Records']:
+        try:
+            process_message(record)
+            # Delete message from queue after processing
+            sqs.delete_message(
+                QueueUrl=youtube_queue_url,  # Directly use the ARN from the record
+                ReceiptHandle=record['receiptHandle']
+            )
+        except Exception as e:
+            print("An unexpected error occurred:", str(e))
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully processed messages from SQS.')
+    }
